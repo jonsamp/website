ed8d7ec405e7d73f415163d3caf564541e32cb9c
jonsamp <sampjon@gmail.com>
2019-09-04 01:44:32 -0400

    Updates

--- app/constants/layout.ts
+++
@@ -1,6 +0,0 @@
-import { Dimensions } from 'react-native';
-
-export const { width: windowWidth } = Dimensions.get('window');
-export const isSmallScreen = windowWidth < 481;
-export const isMediumScreen = windowWidth < 768;
-export const maxWidth = 1200;
